8aacdcc8ab3d6050659cf3669ef261d93a014de4
tushar3736-ui <tusharbainsla62@gmail.com>
2026-04-04 20:53:35 +0530

    code

---
+++ day63.c
@@ -0,0 +1,87 @@
+// Problem: Perform DFS starting from a given source vertex using recursion.
+
+// Input:
+// - n
+// - adjacency list
+// - starting vertex s
+
+// Output:
+// - DFS traversal order
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+// Adjacency list node
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+// Graph structure
+struct Node* adj[MAX];
+int visited[MAX];
+
+// Create new node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Add edge (undirected)
+void addEdge(int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+
+    newNode = createNode(u);
+    newNode->next = adj[v];
+    adj[v] = newNode;
+}
+
+// DFS function
+void DFS(int v) {
+    printf("%d ", v);
+    visited[v] = 1;
+
+    struct Node* temp = adj[v];
+    while (temp != NULL) {
+        if (!visited[temp->vertex]) {
+            DFS(temp->vertex);
+        }
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, m, u, v, s;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    // Initialize
+    for (int i = 0; i < n; i++) {
+        adj[i] = NULL;
+        visited[i] = 0;
+    }
+
+    printf("Enter number of edges: ");
+    scanf("%d", &m);
+
+    printf("Enter edges (u v):\n");
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(u, v);
+    }
+
+    printf("Enter starting vertex: ");
+    scanf("%d", &s);
+
+    printf("DFS Traversal: ");
+    DFS(s);
+
+    return 0;
+}
