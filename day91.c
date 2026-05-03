41db73ceadc299ec0c7b3cb740c3a62d2d32ea6e
tushar3736-ui <tusharbainsla62@gmail.com>
2026-05-03 16:47:07 +0530

    code

--- day91.c
+++ day91.c
@@ -1,67 +1,44 @@
 // Problem: Implement merge sort (divide, sort halves, merge).
 // Print sorted array.
-
-#include <stdio.h>
-
-void merge(int arr[], int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
-
-    int L[n1], R[n2];
-
-    for(int i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for(int j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
-
-    int i = 0, j = 0, k = l;
-
-    while(i < n1 && j < n2) {
-        if(L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
+#include<stdio.h>
+void merge(int arr[], int low, int mid, int high){
+    int left = low ; 
+    int right = mid+1 ; 
+    int ans [high-low+1] ; 
+    int i = 0 ; 
+    while(left<=mid && right<=high){
+        if(arr[left]<arr[right]){
+            ans[i++] = arr[left++] ;  
         }
-        k++;
+        else{
+            ans[i++] = arr[right++] ; 
+        }
     }
-
-    while(i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
+    while(right<=high){
+        ans[i++] = arr[right++] ; 
     }
-
-    while(j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
+    while(left<=mid){
+        ans[i++] = arr[left++] ; 
     }
-}
-
-void mergeSort(int arr[], int l, int r) {
-    if(l < r) {
-        int m = l + (r - l) / 2;
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-        merge(arr, l, m, r);
+    
+    for(int i = low ; i<=high ; i++){
+        arr[i] = ans[i-low] ; 
     }
 }
-
-int main() {
-    int n;
-    scanf("%d", &n);
-
-    int arr[n];
-
-    for(int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-
-    mergeSort(arr, 0, n - 1);
-
-    for(int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-
-    return 0;
+void merge_sort(int arr[], int low, int high){
+    if(low<high){
+        int mid = low + (high-low)/2 ; 
+        merge_sort(arr,low,mid) ; 
+        merge_sort(arr,mid+1,high) ; 
+        merge(arr,low,mid,high) ; 
+    }
+}
+int main(){
+    int n = 5 ; 
+    int arr[] = {5,4,3,2,1} ;  
+    merge_sort(arr,0,n-1) ; 
+    for(int i = 0 ; i<n ; i++){
+        printf("%d ",arr[i]) ; 
+    }
+    return 0 ; 
 }
